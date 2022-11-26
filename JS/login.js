994c844c2e71772608da3d0fcdc6658ad97b6aae
Ragna <leandro__09hotmail.com>
2022-11-26 19:59:25 -0300

    Validacion Mail y Nick, Falta borrar diseñador

--- JS/login.js
+++ JS/login.js
@@ -1,10 +1,64 @@
 const registro = document.getElementById("newAccount")
-const correo = document.getElementById("mail")
+const cuenta = document.getElementById("mail")
 const pass = document.getElementById("contr")
 const init = document.getElementById("btninit")
 const mje = document.getElementById("Advertencia")
 
 
+//traer el array
+let Usuario = JSON.parse(localStorage.getItem("usuario"));
+let Diseñador = JSON.parse(localStorage.getItem("diseñador"));
+//let Sesion = [];
+
+function SearchUser(){
+    //buscar usuario
+    const individuo = Usuario.find((usuario)=>{
+        return usuario.Correo == correo.value;
+        
+    })
+
+
+    if (correo.value == "" || pass.value == "") {
+        mje.innerText = "Los campos no pueden estar vacios"
+    }else{
+    //si usuario existe, comparar la contraseña
+    if (individuo) {
+        let L = Usuario.indexOf(individuo);
+        
+        if (individuo.Contraseña == pass.value) {
+        //window.location.href = "pages/home.html"
+        console.log(Usuario[L].Nick)
+        }else{
+        mje.innerText= "Los datos son incorrectos"
+        }
+    }else{
+        
+        mje.innerText= "Este mail no esta registrado"
+    }}}
+
+function SearchDesigner(){
+        //buscar usuario
+        const individuo = Diseñador.find((diseñador)=>{
+            return diseñador.Correo == correo.value;
+            
+        })
+        if (correo.value == "" || pass.value == "") {
+            mje.innerText = "Los campos no pueden estar vacios"
+        }else{
+        //si usuario existe, comparar la contraseña
+        if (individuo) {
+            let L = Diseñador.indexOf(individuo);
+            
+            if (individuo.Contraseña == pass.value) {
+            //window.location.href = "pages/home.html"
+            console.log(Diseñador[L].Nick)
+            }else{
+            mje.innerText= "Los datos son incorrectos"
+            }
+        }else{
+            
+            mje.innerText= "Este mail no esta registrado"
+        }}}
 
 
 registro.addEventListener("click",(e) =>{
@@ -13,29 +67,20 @@ registro.addEventListener("click",(e) =>{
 
 )
 
-//traer el array
-    let persona = JSON.parse(localStorage.getItem("usuario"))
-;
+
+
+
 init.addEventListener("click", (e)=>{
-    //buscar usuario
-    const individuo = persona.find((usuario)=>{
-        return usuario.Correo == correo.value;
-        
+    const mailIndividuo = Usuario.find((usuario)=>{
+        return usuario.Correo == cuenta.value
     })
-    if (correo.value == "" || pass.value == "") {
-        mje.innerText = "Los campos no pueden estar vacios"
-    }else{
-    //si usuario existe, comparar la contraseña
-    if (individuo) {
-        let L = persona.indexOf(individuo);
-        
-        if (individuo.Contraseña == pass.value) {
-        window.location.href = "pages/home.html"
-        }else{
-        mje.innerText= "Los datos son incorrectos"
-        }
-    }else{
-        
-        mje.innerText= "Este mail no esta registrado"
+    const nickIndividuo = Usuario.find((usuario)=>{
+        return usuario.Nick == cuenta.value
+    })
+    if (mailIndividuo || nickIndividuo) {
+        window.location.href = "pages/home.html";
+    }else
+        mje.innerText="Los datos son incorrectos"
     }
-}})
+    
+)
--- JS/registro.js
+++ JS/registro.js
@@ -1,69 +1,77 @@
 let Usuarios;
 let Diseñador;
+let Perfil;
 
-
-const formulario = document.querySelector(".form1"),
-Name = document.querySelector("#name")
-Lastname = document.querySelector("#lastname")
-Email = document.querySelector("#correo")
-Pass = document.querySelector("#passw")
-parr = document.querySelector("#alert")
+//const formulario = document.querySelector(".form1"),
+const Name = document.querySelector("#name")
+const Lastname = document.querySelector("#lastname")
+const Email = document.querySelector("#correo")
+const Pass = document.querySelector("#passw")
+const parr = document.querySelector("#alert")
 const btn = document.getElementById("newAccount")
 const btnlog = document.getElementById("logbtn")
+const nick = document.getElementById("nickname")
+const Cuenta = document.getElementById("Account")
 
 if (localStorage.getItem("usuario")){
     Usuarios = JSON.parse(localStorage.getItem("usuario"));
 }else{
     Usuarios = [];
 }
-console.log (Usuarios)
+
+
 if (localStorage.getItem("diseñador")){
     Diseñador = JSON.parse(localStorage.getItem("diseñador"));
 }else{
     Diseñador = [];
 }
-console.log(Diseñador)
-
-//construir Usuario
-// class Usuario{
-//     constructor(Name,Lastname,Email,Pass){
-//         this.Name = Nombre;
-//         this.Lastname = Apellido;
-//         this.Email = Correo;
-//         this.Pass = Contraseña;
-//     }
-// }
 
 //limpiar campos
 function clearfield (){
     Name.value=""
     Lastname.value=""
+    nick.value=""
     Email.value=""
     Pass.value=""
 }
 
 
-
-
-
-//almacenar en el local/session
+//almacenar en el local
 function registro(){
-    let persona ={Nombre: Name.value, Apellido: Lastname.value, Correo: Email.value, Contraseña: Pass.value}
-    if (persona.Correo.includes("@coder.com")) {
-        Diseñador.push(persona);
-        localStorage.setItem("diseñador",JSON.stringify(Diseñador));
-        parr.innerText = "Bienvenido Diseñador. Intente iniciar sesion"
-    }else{
-    if (persona.Correo.includes("@hotmail.com")) {
-        Usuarios.push(persona);
-        localStorage.setItem("usuario",JSON.stringify(Usuarios));
-        parr.innerText = "Bienvenido Usuario. Intente iniciar sesion"
-    }else{
-        parr.innerText = "Solo se acepta @coder.com y @hotmail.com"
-    }
+    const mailIndividuo = Usuarios.find((usuario)=>{
+        return usuario.Correo == Email.value
+    })
 
-    }
-}
+    const nickIndividuo = Usuarios.find((usuario)=>{
+        return usuario.Nick == nick.value
+    })
+    
+    if (mailIndividuo){
+        parr.innerText = "Este mail ya fue registrado"
+    }else{
+        if (nickIndividuo) {
+            parr.innerText = "Ese nick ya fue tomado, por favor elija otro"
+        }else{
+        let persona ={Nombre: Name.value, Apellido: Lastname.value,Nick: nick.value, Correo: Email.value, Contraseña: Pass.value}
+        if(individuo){
+            
+        }else{
+        if (persona.Correo.includes("@coder.com")) {
+            Diseñador.push(persona);
+            localStorage.setItem("diseñador",JSON.stringify(Diseñador));
+            parr.innerText = "Bienvenido Diseñador. Intente iniciar sesion"
+            sessionStorage.setItem("perfil",JSON.stringify(Diseñador))
+        }else{
+        if (persona.Correo.includes("@hotmail.com")) {
+            Usuarios.push(persona);
+            localStorage.setItem("usuario",JSON.stringify(Usuarios));
+            parr.innerText = "Bienvenido Usuario. Intente iniciar sesion"
+            sessionStorage.setItem("perfil",JSON.stringify(Usuarios))
+        }else{
+            parr.innerText = "Solo se acepta @coder.com y @hotmail.com"
+        }
+        }
+    }}}}
 
 
 
@@ -76,4 +84,6 @@ btn.addEventListener("click", (e)=>{
 
 btnlog.addEventListener("click",(e)=>{
     window.location.href = "../index.html";
+    
 })
+
