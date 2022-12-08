b1bc5ff65f3b4a6c05de79730df8d57272f74bae
Ragna <leandro__09hotmail.com>
2022-12-08 20:43:57 -0300

    Finalizacion del carrito

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,5 +1,71 @@
-const pepe = document.getElementById("carrito")
+const pintarCarrito =() => {
+    modalCont.innerHTML="";
+    modalCont.style.display = "flex";
+    modalCont.innerHTML = "";    const modalheader = document.createElement("div")
+    modalheader.className = "modalHeader"
+    modalheader.innerHTML =`
+    <h2 class="modalHeaderTitle">Su lista en el carrito:</h2>
+    `
+    modalCont.append(modalheader);
 
-let L = pepe.innerHTML = "<h2>hola</h2>"
+    const modalbutton = document.createElement("botton")
+    
+    modalbutton.innerHTML = "X";
+    modalbutton.className = "modalHeaderButton"
+    modalheader.append(modalbutton)
 
-console.log(L)
+    modalbutton.addEventListener("click",() => {
+        modalCont.style.display = "none";
+    })
+
+
+    Carro.forEach(el => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modalContent"
+        carritoContent.innerHTML = `
+        <img src="${el.img}">
+        <h3>${el.nombre}</h3>
+        <p>$ ${el.precio}</p>
+        <p>Cantidad: ${el.cant}</p>
+        <p>Total: ${el.cant * el.precio}</p>
+        `;
+
+        modalCont.append(carritoContent);
+
+
+
+        let eliminar = document.createElement("button")
+        eliminar.innerText = "❌";
+        eliminar.className = "Eliminar"
+        carritoContent.append(eliminar)
+
+        eliminar.addEventListener("click", eliminarProducto)
+    });
+
+    const total = Carro.reduce((acc, el)=> acc + el.precio * el.cant,0)
+
+    const totBuy= document.createElement("div")
+    totBuy.className = "totalContent";
+    totBuy.innerHTML = `<h2 class="importeTot">Total a pagar: $ ${total}</h2>`;
+    modalCont.append(totBuy);
+    
+};
+
+
+verCarrito.addEventListener("click", pintarCarrito)
+
+const eliminarProducto = () =>{
+    const foundId = Carro.find((element) => element.id)
+
+    Carro = Carro.filter((cartId)=>{
+        return cartId !== foundId;
+    })
+    carritoCounter();
+    pintarCarrito();
+
+}
+
+const carritoCounter = () => {
+    cantCart.style.display= "block"
+    cantCart.innerText = Carro.length
+}
--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,7 @@
-
-let Id = 0;
-let Carro;
-let product;
-
-
+const contenedor = document.getElementById("contenedor");
+const verCarrito = document.getElementById("verCarro");
+const modalCont = document.getElementById("modalContainer");
+const cantidadCarrito = document.getElementById("cantCart")
 
 
 if (localStorage.getItem("carrito")){
@@ -12,71 +10,51 @@ if (localStorage.getItem("carrito")){
     Carro = [];
 }
 
-const tarjeta = document.getElementById("tarjetas");
-const countCart = document.getElementById("contador");
-let Total = 0
-
-Carro.forEach((el)=>{
-    Total = Total + el.Cantidad
-    
-    countCart.innerText = (Total) 
-})
 
 
 fetch('../Json/productos.json')
 .then(Response=> Response.json())
 .then((data =>{
 
-data.forEach((el) => {
-    let prod = document.createElement("div");
-    prod.className = "item";
-    prod.innerHTML = `
-    <h3>${el.nombre}</h3>
-    <img src="${el.img}">
-    <p class="centrar"> $ ${el.precio}</p>
-    `
-    tarjeta.append(prod);
+    data.forEach((el) => {
+        let producto = document.createElement("div");
+        producto.className= "card";
+        producto.innerHTML = `
+        <img src="${el.img}">
+        <h3">${el.nombre}</h3>        
+        <p class="price"> $ ${el.precio}</p>
+    `;
+        contenedor.append(producto);
 
     let btnBuy = document.createElement("button")
-    btnBuy.className="compra";
-    btnBuy.id= Id += 1;
+    btnBuy.className= "comprar";
+    btnBuy.id= el.id;
     btnBuy.innerText= "Agregar al carrito";
-    prod.append(btnBuy);
+    producto.append(btnBuy);
 
+        btnBuy.addEventListener("click",() => {
 
-    btnBuy.addEventListener('click',() => {
+            const repeat = Carro.some((repeatProduct)=> repeatProduct.id === el.id)
+            if (repeat) {
+                Carro.map((produc)=>{
+                    if (produc.id === el.id) {
+                        produc.cant++;
+                    }
+                })
+            }else{
 
-        const ticket = Carro.find((mercad)=>{
-            return data[btnBuy.id-1].nombre == mercad.Desc
-    })
-    if(ticket){
-        let T = Carro.indexOf(ticket)
-        let C = ticket.Cantidad += 1
-        let P = data[btnBuy.id-1].precio * C
-        Carro[T].Cantidad= C
-        Carro[T].Precio= P
-
-        localStorage.setItem("carrito",JSON.stringify(Carro))
-        
-
-}else{
-    let product ={Desc: data[btnBuy.id-1].nombre, Precio: data[btnBuy.id-1].precio, Cantidad: 1}
-    Carro.push(product)
-    localStorage.setItem("carrito",JSON.stringify(Carro))
-
-
-}
-
-Total = 0
-Carro.forEach((el)=>{
-    Total = Total + el.Cantidad
-countCart.innerText = (Total) 
-})
+            Carro.push({
+                id : el.id,
+                img: el.img,
+                nombre: el.nombre,
+                precio: el.precio,
+                cant: el.cant,
+            })}
+            carritoCounter();
+        })
 
 
 
-})})}))
 
-
-
+})}))
 
